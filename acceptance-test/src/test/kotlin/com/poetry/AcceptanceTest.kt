5ae6885a48c92ca6c4d9d94d62addf722807fb58
Anup Kumar <anup.raj1989@gmail.com>
2019-11-16 20:21:14 +0530

    test(Kata-1, Kata-2): acceptance test for Kata-1 and Kata-2

---
+++ acceptance-test/src/test/kotlin/com/poetry/AcceptanceTest.kt
@@ -0,0 +1,24 @@
+package com.poetry
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+
+class AcceptanceTest {
+
+    /*@Test
+    fun `should get verse when asked for poetry`() {
+        val poetryReader: RequestVerse = PoetryReader();
+        val verse = poetryReader.getPoetry();
+        Assertions.assertEquals("Twinkle Twinkle little star", verse);
+    }*/
+
+    /*@Test
+    fun `should be able to get verses when asked for poetry from a poetry library`(@Mock obtainPoem: ObtainPoem) {
+        Mockito.lenient().`when`(obtainPoem.getMeSomePoetry()).thenReturn("Jonny Jonny Yes Pappa")
+        val poetryReader: RequestVerse = PoetryReader(obtainPoem)
+        val verse = poetryReader.getPoetry();
+        Assertions.assertEquals("Jonny Jonny Yes Pappa", verse);
+    }*/
+}

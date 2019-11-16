e6cd635abbec6f0237990efeff045384d303e0c9
Anup Kumar <anup.raj1989@gmail.com>
2019-11-16 23:05:48 +0530

    test(Kata-5): acceptance test for Kata-5

--- acceptance-test/src/test/kotlin/com/poetry/AcceptanceTest.kt
+++ acceptance-test/src/test/kotlin/com/poetry/AcceptanceTest.kt
@@ -2,23 +2,34 @@ package com.poetry
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
 import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
 
+@ExtendWith(MockitoExtension::class)
 class AcceptanceTest {
 
     /*@Test
     fun `should get verse when asked for poetry`() {
-        val poetryReader: RequestVerse = PoetryReader();
-        val verse = poetryReader.getPoetry();
-        Assertions.assertEquals("Twinkle Twinkle little star", verse);
+        val poetryReader: RequestVerse = PoetryReader()
+        val verse = poetryReader.getPoetry()
+        Assertions.assertEquals("Twinkle Twinkle little star", verse)
     }*/
 
     /*@Test
     fun `should be able to get verses when asked for poetry from a poetry library`(@Mock obtainPoem: ObtainPoem) {
         Mockito.lenient().`when`(obtainPoem.getMeSomePoetry()).thenReturn("Jonny Jonny Yes Pappa")
         val poetryReader: RequestVerse = PoetryReader(obtainPoem)
-        val verse = poetryReader.getPoetry();
-        Assertions.assertEquals("Jonny Jonny Yes Pappa", verse);
+        val verse = poetryReader.getPoetry()
+        Assertions.assertEquals("Jonny Jonny Yes Pappa", verse)
+    }*/
+
+    /*@Test
+    fun `should be able to get verses when asked for poetry from a poetry file library`() {
+        val obtainPoem: ObtainPoem = PoemFileAdapter()
+        val poetryReader: RequestVerse = PoetryReader(obtainPoem)
+        val verse = poetryReader.getPoetry()
+        Assertions.assertEquals("Humpty dumpty sat on a wall!", verse)
     }*/
 }
